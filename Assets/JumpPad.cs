d2bb889b50d1460de3e51d3519fa10aab9789f5f
Jean Louis Deurveilher <60738892+Narraxxas@users.noreply.github.com>
2024-01-23 19:57:28 +0100

    jl - feat : Adding JumpPad system

---
+++ Assets/JumpPad.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpPad : MonoBehaviour
+{
+    [SerializeField] float bouncePower = 20f;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<Rigidbody2D>().AddForce(transform.up * bouncePower, ForceMode2D.Impulse);
+    }
+}
