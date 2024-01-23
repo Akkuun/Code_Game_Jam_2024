afb8800602634bd7e6a3120e1f94379fe0ffad85
Jean Louis Deurveilher <60738892+Narraxxas@users.noreply.github.com>
2024-01-23 09:12:38 +0100

    jl - feat : Double jump system

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,28 +2,39 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    // Movement bases
     private float horizontal;
-    private float speed = 8f;
-    private float jumpingPower = 16f;
+    private float speed = 10f;
+    private float jumpingPower = 12f;
     private bool isFacingRight = true;
+    private bool doubleJumpEnabled = true;
 
-    [SerializeField] private Rigidbody2D rb;
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private LayerMask groundLayer;
+
+    [SerializeField] private Rigidbody2D rb; // Ref for player's rigidBody
+    [SerializeField] private Transform groundCheck; // Position of player's foots
+    [SerializeField] private LayerMask groundLayer; // Layer to collide and check for isGrounded
 
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        if (Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump"))
         {
-            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            if (IsGrounded() || doubleJumpEnabled)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+                if (!IsGrounded())
+                    doubleJumpEnabled = false;
+            }
         }
 
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
         }
+
+        if (IsGrounded() && !doubleJumpEnabled)
+            doubleJumpEnabled = true;
 
         Flip();
     }
