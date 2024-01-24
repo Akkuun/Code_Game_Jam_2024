f0627ea483e0b903c145cda86fafbe27c31fb8a0
thi nguyen <thichristine@icloud.com>
2024-01-24 04:23:21 +0100

    ajout input enigme

--- Assets/Scripts/ReadInput.cs
+++ Assets/Scripts/ReadInput.cs
@@ -1,26 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Compilation;
 using UnityEngine;
 
 public class ReadInput : MonoBehaviour
 {
-
-    private string input; 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
+    private string reponse = "315"; 
+  
 
     public void readString(string s)
     {
-        input = s;
-        Debug.Log(input); 
+
+        if (s == reponse)
+            Debug.Log(s);
+
     }
 }
---
+++ Assets/Scripts/ShowInput.cs
@@ -0,0 +1,46 @@
+    using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime;
+using UnityEngine;
+
+public class ShowInput : MonoBehaviour
+{
+
+    public GameObject input;
+    private bool collide = false;
+
+
+    void Start()
+    {
+        input.SetActive(false);
+    }
+
+    void Update()
+    {
+
+        
+        if (collide && Input.GetKeyDown(KeyCode.T))
+        {
+           
+            input.SetActive(true);
+        }
+
+    }
+
+     private void OnTriggerEnter2D(Collider2D other)
+     {
+        Debug.Log("test");
+        collide = true;
+
+     }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        collide = false;
+        input.SetActive(false);
+    }
+
+
+
+
+}
